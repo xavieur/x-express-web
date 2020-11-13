c4592d4005b23ad42fce182806a25afdfd118b73
xavieur <one@email.com>
2020-11-13 23:57:35 +0100

    fix: user.save

--- routers/user.js
+++ routers/user.js
@@ -6,7 +6,7 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
-        await User.save()
+        await user.save()
         res.status(201).send(user)
     } catch (e) {
         console.log(e)
