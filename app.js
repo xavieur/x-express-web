7e2a7b18acb6ff7df6eacfec0b8e6e6a075a213a
xavieur <one@email.com>
2020-11-13 08:35:36 +0100

    feat: tasks connection

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require('express')
+
+require('./db/mongoose')
+const taskRouter = require('./routers/task')
+
+const port = process.env.PORT
 
 // express app
 const app = express();
 
 // listen for requests
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server listening to port ${port}`)
+});
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -21,13 +28,25 @@ app.use((req, res, next) => {
 });
 
 
+app.get('/canciones', (req, res) => {
+  res.render('canciones', {titulo: 'Canciones'})
+})
+app.get('/canticos', (req, res) => {
+  res.render('canticos', {titulo: 'Cánticos'})
+})
+app.get('/cantos', (req, res) => {
+  res.render('cantos', {titulo: 'Cantos'})
+})
+
+
+
 app.get('/', (req, res) => {
   const blogs = [
-    {title: 'Pastel de zanahoria', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Zhulien de champiñones', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Shakshuka', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    { title: 'Pastel de zanahoria', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'Zhulien de champiñones', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'Shakshuka', snippet: 'Lorem ipsum dolor sit amet consectetur' },
   ];
-  res.render('index', { title: 'Home', blogs });
+  res.render('index', { title: 'Home', blogs:blogs });
 });
 
 app.get('/about', (req, res) => {
@@ -38,7 +57,35 @@ app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
 
+app.get('/number', (req, res) => {
+  res.render('numeros', {})
+})
+
+app.get('/nov', (req, res) => {
+  res.render('novedades', {})
+})
+
+app.post('/songs', (req, res)=>{
+
+})
+app.get('/songs', (req, res)=>{
+  
+})
+app.get('/songs/:id', (req, res)=>{
+  
+})
+app.patch('/songs/:id', (req, res)=>{
+  
+})
+app.delete('/songs/:id', (req, res)=>{
+
+})
+
+app.use(express.json())
+app.use(taskRouter)
+
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
