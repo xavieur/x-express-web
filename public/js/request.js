32f4c2f51b0ddc11f24b1cba37d195946149c8ee
xavieur <one@email.com>
2020-11-17 23:00:30 +0100

    feat: axios

--- public/js/request.js
+++ public/js/request.js
@@ -1,1 +1,17 @@
-alert('request')
+const form = document.getElementById('test-form')
+
+form.addEventListener('click', (e) => {
+    e.preventDefault
+    const data = {
+        question: e.target.elements.question.value,
+        answer1: e.target.elements.answer1.value,
+        answer2: e.target.elements.answer2.value,
+        answer3: e.target.elements.answer3.value,
+        answer4: e.target.elements.answer4.value
+    }
+    axios({
+        method: 'post',
+        url: '/api/tests',
+        data
+    })
+})
