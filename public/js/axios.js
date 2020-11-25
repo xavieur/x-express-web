a3f671284cb97ea1950d2b812ae2a983343bd22b
xavieur <one@email.com>
2020-11-25 12:02:17 +0100

    test

--- public/js/axios.js
+++ public/js/axios.js
@@ -4,7 +4,7 @@ const readRequest = document.querySelector('#read')
 const updateRequest = document.querySelector('#update')
 const deleteRequest = document.querySelector('#delete')
 
-const testPrueba = {
+const testData = {
     question: '¿quién ha llamado?',
     answer1: 'Juan',
     answer2: 'Jesús',
@@ -14,7 +14,7 @@ const testPrueba = {
 
 postRequest.addEventListener('click', async (e) => {
     e.preventDefault()
-    const resultado = await axios.post('https://x-express-web.herokuapp.com/api/tests', { data: testPrueba })
+    const resultado = await axios.post('https://x-express-web.herokuapp.com/api/tests', testData)
 })
 
 readRequests.addEventListener('click', async (e) => {
@@ -32,7 +32,7 @@ readRequest.addEventListener('click', async (e) => {
 
 updateRequest.addEventListener('click', async (e) => {
     e.preventDefault()
-    const resultado = await axios.patch('https://x-express-web.herokuapp.com/api/tests/5fb63caff287430989eddb04', {data: {question:'¿Cómo se traduce al castellano en nombre del módulo que gestiona la base de datos?'}})
+    const resultado = await axios.patch('https://x-express-web.herokuapp.com/api/tests/5fb63caff287430989eddb04', {question:'¿Cómo se traduce al castellano en nombre del módulo que gestiona la base de datos?'})
     console.log(resultado)
 
 })
