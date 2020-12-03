a24a36d1f4089259237c94de75060ff4d1c33413
xavieur <one@email.com>
2020-12-03 10:17:59 +0100

    feat: bcrypt

--- models/user.js
+++ models/user.js
@@ -47,6 +47,7 @@ userSchema.pre('save', async function(next){
     if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password, 8)
     }
+    console.log('hola desde pre')
     next()
 })
 
