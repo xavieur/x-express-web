50496e7dbda905d0dc49fa948defec2fb2323af5
xavieur <one@email.com>
2020-12-27 11:24:37 +0100

    fix: remove polluted files

--- public/js/exam.js
+++ public/js/exam.js
@@ -1,28 +1,26 @@
 for (let form of document.forms) {
-    console.log(form)
     form.addEventListener('click', (e) => {
         const id = e.target.parentNode.id
         const value = parseInt(e.target.value)
-        console.log(id) // el id de la pregunta
-        console.log(value) // el value de cada respuesta
         if (id && value) {
             const guesses = readGuesses()
-            console.log('guesses before push ', guesses)
-            guesses.push({ id, value })
-            console.log('guesses after push ', guesses)
-            writeGuesses(guesses)
+            if (guesses.findIndex((guess) => {
+                return guess.id === id
+            }) === -1) {
+                guesses.push({ id, value })
+                writeGuesses(guesses)
+            }
         }
     })
 }
 
 const consultRightAnswers = async () => {
-    console.log('hola q tal?')
-    let answers = await axios.get('https://x-express-web.herokuapp.com/api/tests')
-    console.log(answers.data)
+    let tests = await axios.get('https://x-express-web.herokuapp.com/api/tests')
 
-    answers = answers.data.map((test)=>{
-        return {id:test._id, value:test.value}
+    const answers = tests.data.map((test) => {
+        return { id: test._id, value: test.solution }
     })
+
     writeAnswers(answers)
 }
 
@@ -41,7 +39,6 @@ const writeGuesses = (guesses) => {
     localStorage.setItem('guesses', JSON.stringify(guesses))
 }
 
-
 const readAnswers = () => {
     const answers = localStorage.getItem('answers')
     try {
@@ -50,19 +47,76 @@ const readAnswers = () => {
         return []
     }
 }
+
 const writeAnswers = (answers) => {
     localStorage.setItem('answers', JSON.stringify(answers))
 }
 
-console.log(readGuesses())
-
-const checkAnswers = () => {
-    const guesses = readGuesses()
-    const answers = readAnswers()
-
-
+const checkAnswers = async () => {
+    const guesses = await readGuesses()
+    const answers = await readAnswers()
+    let rights = 0
+    let wrongs = 0
+    guesses.forEach((guess) => {
+        const answer = answers.find((answer) => {
+            return answer.id === guess.id
+        })
+        if (answer.value === guess.value) {
+            document.getElementById(guess.id).classList.add('right-answer')
+            rights++
+        } else {
+            document.getElementById(guess.id).classList.add('wrong-answer')
+            /* document.querySelector(`#${guess.id} span:nth-child(${guess.value})`).classList.add('wrong-answer') */
+            wrongs++
+        }
+    })
+    return { rightAnswersNumber: rights, wrongAnswersNumber: wrongs, total: answers.length }
 }
 
-window.addEventListener('unload', (e)=>{
+window.addEventListener('unload', (e) => {
     localStorage.clear()
+    for(let form of document.forms){
+        form.reset()
+    }
+})
+
+const showAnswersButton = document.querySelector('#verify')
+const resetButton = document.querySelector('#reset')
+const answersModal = document.querySelector('#answersModal')
+const closeAnswersModal = document.querySelector('#answersModal span.close')
+
+const rightAnswers = document.querySelector('#rightAnswers')
+const wrongAnswers = document.querySelector('#wrongAnswers')
+const score = document.querySelector('#score')
+
+resetButton.addEventListener('click', (e) => {
+    localStorage.clear()
+    for(let form of document.forms){
+        console.log('reset button -> form', form)
+        form.classList.remove('right-answer', 'wrong-answer')
+        form.reset()
+    }
+})
+
+showAnswersButton.addEventListener('click', async (e) => {
+    e.preventDefault()
+
+    const { rightAnswersNumber, wrongAnswersNumber, total } = await checkAnswers()
+    const scoreNumber = (rightAnswersNumber ? (rightAnswersNumber - wrongAnswersNumber * .33) * 10 / total : 0).toFixed(2)
+
+    rightAnswers.textContent = rightAnswersNumber
+    wrongAnswers.textContent = wrongAnswersNumber
+    score.textContent = scoreNumber
+
+    answersModal.classList.add('show')
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target === answersModal) {
+        answersModal.classList.remove('show')
+    }
+})
+
+closeAnswersModal.addEventListener('click', (e) => {
+    answersModal.classList.remove('show')
 })
